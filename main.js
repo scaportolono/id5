23a02b1cfae783f0a2c6ac505dff5a72c0712b38
scaportolono <scaportolono@gmail.com>
2019-04-04 22:09:28 +0900

    css

--- main.js
+++ main.js
@@ -1,16 +1,62 @@
-console.log("hello")
 
-
-let getJson = () => {
-    var xmlHttpRequest = new XMLHttpRequest();
-
-    xmlHttpRequest.onreadystatechange = () => {
-        if (xmlHttpRequest.readyState === 4 || xmlHttpRequest.status === 200) {
-
-            console.log("get");
-        }
-    }
-
+let loadMapImage = () => {
 }
 
-getJson();
+
+// 以下データ
+let data =  {
+  "maps": [
+    {
+      "id": 0,
+      "patterns": [
+        {
+          "type": "a",
+          "hunter": {
+            "x": 0,
+            "y": 2
+          },
+          "survivors": [
+            {
+              "x": 0,
+              "y": 2
+            },
+            {
+              "x": 0,
+              "y": 2
+            },
+            {
+              "x": 0,
+              "y": 2
+            },
+            {
+              "x": 0,
+              "y": 2
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "patterns": [
+      ]
+    },
+    {
+      "id": 2,
+      "patterns": [
+      ]
+    },
+    {
+      "id": 3,
+      "patterns": [
+      ]
+    },
+    {
+      "id": 4,
+      "patterns": [
+      ]
+    }
+  ]
+}
+
+loadMapImage();
