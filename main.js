c90647025b17b854d8f3ea2b88f3abcf73f46d52
scaportolono <scaportolono@gmail.com>
2019-04-05 01:26:26 +0900

    update

--- main.js
+++ main.js
@@ -1,60 +1,54 @@
+// random
 
-
-
-
-// 以下データ
-let data =  {
-  "maps": [
-    {
-      "id": 0,
-      "patterns": [
-        {
-          "type": "a",
-          "hunter": {
-            "x": 0,
-            "y": 2
-          },
-          "survivors": [
-            {
-              "x": 0,
-              "y": 2
-            },
-            {
-              "x": 0,
-              "y": 2
-            },
-            {
-              "x": 0,
-              "y": 2
-            },
-            {
-              "x": 0,
-              "y": 2
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "id": 1,
-      "patterns": [
-      ]
-    },
-    {
-      "id": 2,
-      "patterns": [
-      ]
-    },
-    {
-      "id": 3,
-      "patterns": [
-      ]
-    },
-    {
-      "id": 4,
-      "patterns": [
-      ]
-    }
-  ]
+// timelimit 
+let timelimit = (time) => {
 }
 
+// マップ画像の縮小比率
+let ratio = () => {
+}
+
+// スポーン位置の描画
+let spawn = () => {
+    draw(0,0)
+}
+
+// クリックイベント
+let mapClick = (e) => {
+    // 座標とって返す
+    return 0
+}
+
+// _h,_sの座標設定
+let draw = (x,y) => {
+}
+
+// load
+// マップの選出（軍需工場）
+// -> 選択以外はdisplay:none
+//
+// map選択
+// マップの選出（value=5なら全マップからランダム)
+// -> 選択以外はdisplay:none
+//
+// start 
+// startボタンのdisabled
+//
+// マップの選出（ランダムの場合)
+// -> 選択以外はdisplay:none
+//
+// パターンの選出
+// expectedの描画（display;Noneのまま)
+// answer_hunter描画
+//
+// カウントダウン開始
+// 画像クリックでその位置に印をつける（4回まで)
+// -> answerの座標設定
+// カウントダウンが終わるか、クリック数が４回終わるかで終了
+//
+// クリックイベントの廃棄
+// クリックカウントのリセット
+// カウントダウンが途中なら破棄
+// expectedをdisplay:blockに
+
+console.info(data)
