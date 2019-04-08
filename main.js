814b2c8441a588e9072cb0adb247755ab307b322
scaportolono <scaportolono@gmail.com>
2019-04-08 23:33:28 +0900

    t

--- main.js
+++ main.js
@@ -18,19 +18,51 @@ let mapClick = (e) => {
     // 座標とって返す
     return 0
 }
+// sample
+elem = document.querySelector("#map0")
+rect = elem.getBoundingClientRect();
+var positionX = rect.left + window.pageXOffset;
+var positionY = rect.top + window.pageYOffset;
+spawn = document.getElementById("map0_answer_s0")
+
+elem.addEventListener('click', (evt)=>{
+spawn.classList.add("answerd")
+spawn.style.left = evt.pageX -  positionX - 5 + "px"
+spawn.style.top = evt.pageY -  positionY - 5 + "px"
+
+
+console.log(evt.pageX -  positionX);
+})
 
 // _h,_sの座標設定
 let draw = (x,y) => {
 }
 
-// load
-// マップの選出（軍需工場）
-// -> 選択以外はdisplay:none
-//
-// map選択
-// マップの選出（value=5なら全マップからランダム)
-// -> 選択以外はdisplay:none
-//
+// map 表示
+const mapSelect = document.querySelector("#mapSelect");
+mapSelect.onchange = () => {
+    // ランダム以外
+    if (mapSelect.value<5){
+        showMap(mapSelect.value);
+    }
+}
+let showMap = (mapID) => {
+    // すべてhidden
+    maps = document.querySelectorAll(".mapfield");
+    for(i=0; i<maps.length; i++){
+        maps[i].classList.add("hidden");
+    }
+    // 指定IDをshow
+    if (mapID<5){
+        maps[mapID].classList.remove("hidden");
+    } else {
+        maps[0].classList.remove("hidden");
+    }
+}
+
+// start
+let start = () => {
+    console.log("start...");
 // start 
 // startボタンのdisabled
 //
@@ -46,9 +78,41 @@ let draw = (x,y) => {
 // -> answerの座標設定
 // カウントダウンが終わるか、クリック数が４回終わるかで終了
 //
+
+}
+
+// end
+let end = () => {
 // クリックイベントの廃棄
 // クリックカウントのリセット
 // カウントダウンが途中なら破棄
 // expectedをdisplay:blockに
+}
+
+// 
+
+// load(初期表示)
+window.onload = () => {
+    // 工場初期表示
+    showMap(0);
+}
+
+
+
+
+
+
+// カウントダウンの値取得
+// CSSclassセット
+// settimeoutセット
+// カウントダウン後にアクション
+// 途中でカウントダウンを終了させる
+
+// 比率
+/* 
+	画像オリジナルサイズを取得（json）
+  表示サイズを取得
+  比率を出す
+*/
 
 console.info(data)
