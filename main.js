9f26a65817d37db81d472a8da8d83cbdff8da7ce
scaportolono <scaportolono@gmail.com>
2019-04-09 02:03:08 +0900

    t

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let current = {
+    mapElem: {},
+    mapID: 0,
+    mapRect: {},
+    clickCount: 0
+}
 // random
 
 // timelimit 
@@ -6,36 +12,41 @@ let timelimit = (time) => {
 
 // マップ画像の縮小比率
 let ratio = () => {
+// 比率
+/* 
+	画像オリジナルサイズを取得（json）
+  表示サイズを取得
+  比率を出す
+*/
 }
 
-// スポーン位置の描画
-let spawn = () => {
-    draw(0,0)
-}
+// 画像上のスポーン座標
+let spawn = (clickX, clickY) => {
+    rect = current.mapRect
+    positionX = rect.left + window.pageXOffset;
+    positionY = rect.top + window.pageYOffset;
 
+    return {
+        x: clickX - positionX,
+        y: clickY - positionY
+    }
+}
+// _h,_sの座標設定
+let draw = (elem,x,y) => {
+    elem.classList.add("answerd")
+    elem.style.left = x - 5 + "px"
+    elem.style.top = y - 5 + "px"
+}
 // クリックイベント
 let mapClick = (e) => {
     // 座標とって返す
-    return 0
-}
-// sample
-elem = document.querySelector("#map0")
-rect = elem.getBoundingClientRect();
-var positionX = rect.left + window.pageXOffset;
-var positionY = rect.top + window.pageYOffset;
-spawn = document.getElementById("map0_answer_s0")
-
-elem.addEventListener('click', (evt)=>{
-spawn.classList.add("answerd")
-spawn.style.left = evt.pageX -  positionX - 5 + "px"
-spawn.style.top = evt.pageY -  positionY - 5 + "px"
-
-
-console.log(evt.pageX -  positionX);
-})
-
-// _h,_sの座標設定
-let draw = (x,y) => {
+    let { x, y } = spawn(e.pageX, e.pageY)
+    count = current.clickCount
+    draw(document.getElementById("map" + current.mapID + "_answer_s" + count), x, y)
+    current.clickCount++;
+    if (current.clickCount === 4) {
+        end();
+    }
 }
 
 // map 表示
@@ -43,76 +54,72 @@ const mapSelect = document.querySelector("#mapSelect");
 mapSelect.onchange = () => {
     // ランダム以外
     if (mapSelect.value<5){
-        showMap(mapSelect.value);
+       setMap(mapSelect.value);
     }
 }
-let showMap = (mapID) => {
-    // すべてhidden
+let setMap = (mapID) => {
+    // すべてhidden&イベントの削除
     maps = document.querySelectorAll(".mapfield");
     for(i=0; i<maps.length; i++){
         maps[i].classList.add("hidden");
+        maps[i].removeEventListener("click", mapClick);
     }
-    // 指定IDをshow
+    // 指定IDをshow&currentの設定
     if (mapID<5){
         maps[mapID].classList.remove("hidden");
+        current.mapElem = maps[mapID];
+        current.mapID = mapID;
     } else {
         maps[0].classList.remove("hidden");
+        current.mapElem = maps[0];
+        current.mapID = 0;
     }
+    // currentMapの座標
+    current.mapRect = current.mapElem.getBoundingClientRect();
 }
 
 // start
 let start = () => {
     console.log("start...");
-// start 
-// startボタンのdisabled
-//
-// マップの選出（ランダムの場合)
-// -> 選択以外はdisplay:none
-//
-// パターンの選出
-// expectedの描画（display;Noneのまま)
-// answer_hunter描画
-//
-// カウントダウン開始
-// 画像クリックでその位置に印をつける（4回まで)
-// -> answerの座標設定
-// カウントダウンが終わるか、クリック数が４回終わるかで終了
-//
 
+    // startボタンのdisabled
+    //
+    // ランダムの場合Mapをsetし直す
+    //
+    // スポーンパターンのset 
+        // expectedの描画（display;Noneのまま)
+        // answer_hunter描画
+
+    // カウントダウン開始
+    // カウントダウンの値取得
+    // CSSclassセット
+    // settimeoutセット
+    // カウントダウン後にアクション
+    // 途中でカウントダウンを終了させる
+
+    // 画像クリックでその位置に印をつける（4回まで)
+    // -> answerの座標設定
+    current.mapElem.addEventListener('click', mapClick)
+
+    // カウントダウンが終わるか、クリック数が４回終わるかで終了
 }
 
 // end
 let end = () => {
-// クリックイベントの廃棄
-// クリックカウントのリセット
-// カウントダウンが途中なら破棄
-// expectedをdisplay:blockに
+    // クリックイベントの廃棄
+    current.mapElem.removeEventListener("click", mapClick);
+    // クリックカウントのリセット
+    current.clickCount = 0;
+    // カウントダウンが途中なら破棄
+    // expectedをdisplay:blockに
+    console.log("end...");
 }
-
-// 
 
 // load(初期表示)
 window.onload = () => {
     // 工場初期表示
-    showMap(0);
+    setMap(0);
+    document.querySelector("#start").addEventListener("click", start);
 }
-
-
-
-
-
-
-// カウントダウンの値取得
-// CSSclassセット
-// settimeoutセット
-// カウントダウン後にアクション
-// 途中でカウントダウンを終了させる
-
-// 比率
-/* 
-	画像オリジナルサイズを取得（json）
-  表示サイズを取得
-  比率を出す
-*/
 
 console.info(data)
